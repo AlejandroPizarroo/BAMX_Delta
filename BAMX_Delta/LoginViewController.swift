7a83d69aec0955851dc5f5284b089452dd379979
AlejandroPizarroCh <A01633784@tec.mx>
2022-10-15 18:03:13 -0500

    Sign In and Register interfaces

---
+++ BAMX_Delta/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  BAMX_Delta
+//
+//  Created by Alejandro Pizarro on 15/10/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BAMX_Delta/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  BAMX_Delta
+//
+//  Created by Alejandro Pizarro on 15/10/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
