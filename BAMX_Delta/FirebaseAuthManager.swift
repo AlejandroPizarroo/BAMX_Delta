a604962d2fd422bbb4d883511726b35278926fbd
AlejandroPizarroCh <A01633784@tec.mx>
2022-11-07 20:07:38 -0600

    Registro usuarios a Firebase e implementación de autenticación y UUID

---
+++ BAMX_Delta/FirebaseAuthManager.swift
@@ -0,0 +1,40 @@
+//
+//  FirebaseAuthManager.swift
+//  BAMX_Delta
+//
+//  Created by Alejandro Pizarro on 07/11/22.
+//
+
+import FirebaseAuth
+
+class FirebaseAuthManager{
+    
+    func createUser(email: String, password: String, completionBlock: @escaping (_ success: Bool) -> Void){
+        
+        Auth.auth().createUser(withEmail: email, password: password) {
+            
+            (authResult, error) in
+            
+            if let usuario = authResult?.user{
+                print(usuario)
+                completionBlock(true)
+            }else{
+                completionBlock(false)
+            }
+        }
+    }
+    
+    func logIn(email:String, password: String, completionBlock: @escaping (_ success: Bool) -> Void){
+        
+        Auth.auth().signIn(withEmail: email, password: password){
+            (authResult, error) in
+            if let usuario = authResult?.user{
+                print(usuario)
+                completionBlock(true)
+            }else{
+                completionBlock(false)
+            }
+        }
+    }
+    
+}
--- BAMX_Delta/RegisterViewController.swift
+++ BAMX_Delta/RegisterViewController.swift
@@ -21,21 +21,68 @@ class RegisterViewController: UIViewController {
     
     let ref = Database.database().reference(withPath: "Usuarios")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+// MARK: Area de funciones
 
-    @IBAction func saveUser(_ sender: Any) {
+    func displayAlertMessage(message:String){
+        let alertController = UIAlertController(title: "Mensaje de la aplicación", message: message, preferredStyle: .alert)
         
-        let key = self.textName.text!
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func verifyEmail(email: String) -> Bool{
+            let emailRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+            
+            let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+            
+            if emailPredicate.evaluate(with: textEmail.text!)
+            {
+                return true
+            }
+            else{
+                return false
+            }
+        }
+    
+    @IBAction func saveUser(_ sender: Any) {
+    
+// MARK: Registrar usuario
+        
+        let key = UUID().uuidString
         let object : [String:Any] = [
             "nombre": self.textName.text!,
             "apellido": self.textApellido.text!,
             "telefono": self.textPhone.text!
         ]
         
-        // agregar usuarios a firebase
+        if textEmail.text?.isEmpty == true || textPassword.text?.isEmpty == true || textName.text?.isEmpty == true || textApellido.text?.isEmpty == true || textPhone.text?.isEmpty == true   {
+            displayAlertMessage(message: "Tienes campos de texto vacíos")
+        }else{
+            if verifyEmail(email: textEmail.text!){
+                let sgManager = FirebaseAuthManager()
+                
+                sgManager.createUser(email: textEmail.text!, password: textPassword.text!){
+                    [weak self] (success) in
+                    
+                    var message = ""
+                    
+                    guard let `self` = self else {return}
+                    
+                    if success{
+                        message = "El usuario se creó exitosamente"
+                    }else{
+                        message = "Hubo un problema al crear el usuario"
+                    }
+                    
+                    self.displayAlertMessage(message: message)
+                }
+            }else{
+                self.displayAlertMessage(message: "El email es incorrecto!")
+            }
+            
+        }
+        
         ref.child(key).setValue(object){
             (error: Error?, ref:DatabaseReference) in
                     
@@ -46,10 +93,10 @@ class RegisterViewController: UIViewController {
                         print(error)
             }
             else{
-                message = "El usuario se agregó exitosamente!"
+                message = "El usuario se creó exitosamente!"
             }
                 
-            let alertController = UIAlertController(title: "Mensaje de la aplición", message: message, preferredStyle: .alert)
+            let alertController = UIAlertController(title: "Mensaje de la aplición ", message: message, preferredStyle: .alert)
                     
             let OKAction = UIAlertAction(title: "OK", style: .default)
                 {(action: UIAlertAction!) in
@@ -60,6 +107,8 @@ class RegisterViewController: UIViewController {
                     
             self.present(alertController, animated: true, completion: nil)
         }
+        
+        
     }
 }
     
