7182ab56947ed900f6efe021d47470eefb7e1685
AlejandroPizarroCh <A01633784@tec.mx>
2022-11-16 12:52:51 -0600

    Video Playar y pantalla de About Us

--- BAMX_Delta/AboutUsViewController.swift
+++ BAMX_Delta/AboutUsViewController.swift
@@ -5,14 +5,25 @@
 //  Created by Alejandro Pizarro on 14/11/22.
 //
 
+import youtube_ios_player_helper
 import Foundation
 import UIKit
 
 class AboutUsViewController: UIViewController {
+    
+    @IBOutlet var playerView : YTPlayerView!
 
+    @IBOutlet weak var aboutUs: UIButton!
+    @IBOutlet weak var campaigns: UIButton!
+    @IBOutlet weak var donations: UIButton!
+    @IBOutlet weak var myAccount: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playerView.load(withVideoId: "p38WgakuYDo")
     }
 
 
