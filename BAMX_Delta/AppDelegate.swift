0ba25f95174fcb19a82a0fe12c6469993abbeb3d
AlejandroPizarroCh <A01633784@tec.mx>
2022-11-26 23:30:50 -0600

    Integración PayPal

--- BAMX_Delta/AppDelegate.swift
+++ BAMX_Delta/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import Braintree
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        BTAppContextSwitcher.setReturnURLScheme("mx.itesm.BAMX-Delta.payments")
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
@@ -32,6 +34,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if url.scheme?.localizedCaseInsensitiveCompare("mx.itesm.BAMX-Delta.payments") == .orderedSame {
+            return BTAppContextSwitcher.handleOpenURL(url)
+        }
+        return false
     }
 
 
--- BAMX_Delta/DonationsViewController.swift
+++ BAMX_Delta/DonationsViewController.swift
@@ -7,13 +7,50 @@
 
 import Foundation
 import UIKit
+import Braintree
+
 
 class DonationsViewController: UIViewController {
+    
+    var braintreeClient: BTAPIClient?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //braintreeClient = BTAPIClient(authorization: "sandbox_6m27d6hd_fdswynbdc3kggfwz")!
+        
+        
     }
 
+    @IBAction func paypalPaymentProcess(_ sender: Any) {
+        // Example: Initialize BTAPIClient, if you haven't already
+        braintreeClient = BTAPIClient(authorization: "sandbox_6m27d6hd_fdswynbdc3kggfwz")!
+        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient!)
 
+        // Specify the transaction amount here. "2.32" is used in this example.
+        let request = BTPayPalCheckoutRequest(amount: "2.32")
+        request.currencyCode = "MXN" // Optional; see BTPayPalCheckoutRequest.h for more option
+        
+        payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
+            if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+                print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+
+                // Access additional information
+                let email = tokenizedPayPalAccount.email
+                print(email!)
+                let firstName = tokenizedPayPalAccount.firstName
+                let lastName = tokenizedPayPalAccount.lastName
+                let phone = tokenizedPayPalAccount.phone
+
+                // See BTPostalAddress.h for details
+                let billingAddress = tokenizedPayPalAccount.billingAddress
+                let shippingAddress = tokenizedPayPalAccount.shippingAddress
+            } else if let error = error {
+                print(error)// Handle error here...
+            } else {
+                // Buyer canceled payment approval
+            }
+        }
+    }
+    
 }
